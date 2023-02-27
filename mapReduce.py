24a01f4e476a43c234bdfa10875eb799cd856799
tanukansalgit <tanukansal167@gmail.com>
2023-02-27 04:15:47 -0500

    making user api flexible

--- mapReduce.py
+++ mapReduce.py
@@ -1,15 +1,17 @@
 from master import Master
 
 class MapReduce:
-    def __init__(self, mappers, reducers, filePaths):
+    def __init__(self, mappers, reducers, filePaths, outputCountFilePath, outputInvertedIndexFilePath):
         self.nMappers = mappers
         self.nReducers = reducers
         self.filePaths = filePaths
         self.fileMaxSize = 10000
+        self.outputCountFile = outputCountFilePath
+        self.outputInvertedIndexFile = outputInvertedIndexFilePath
         pass
 
     def initialiseMaster(self):
-        master1 = Master(self.nMappers, self.nReducers, self.filePaths, self.fileMaxSize)
+        master1 = Master(self.nMappers, self.nReducers, self.filePaths, self.fileMaxSize, self.outputCountFile, self.outputInvertedIndexFile)
         master1.start()
         master1.join()
         pass
@@ -24,7 +26,9 @@ if __name__ == "__main__":
     # "/Users/tanukansal/Documents/distributedSystems/MapReduce-Distributed-Systems/input/file4.txt",
     # "/Users/tanukansal/Documents/distributedSystems/MapReduce-Distributed-Systems/input/file5.txt"
     ]
+    outputCountFilePath = 'count-output.txt'
+    outputInvertedIndexFilePath = 'inverted-index-output.txt'
 
-    mapReduce = MapReduce(mappers, reducers, filePaths)
+    mapReduce = MapReduce(mappers, reducers, filePaths, outputCountFilePath, outputInvertedIndexFilePath)
     mapReduce.initialiseMaster()
 
